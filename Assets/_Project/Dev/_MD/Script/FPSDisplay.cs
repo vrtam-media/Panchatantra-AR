020f8a4cdccb539c2a7183747980314693c8810f
MDx-Alpha <sanjumugu@gmail.com>
2026-02-17 23:41:24 +0530

    update: fps logger

--- Assets/_Project/Dev/_MD/Script/FPSDisplay.cs
+++ Assets/_Project/Dev/_MD/Script/FPSDisplay.cs
@@ -1,28 +1,60 @@
 using UnityEngine;
-using TMPro;
+using UnityEngine.Profiling;
 
 public class FPSDisplay : MonoBehaviour
 {
-    private TextMeshProUGUI fpsText;
     private float deltaTime;
+    private float fps;
+    private float frameTimeMs;
 
-    void Start()
+    private GUIStyle style;
+    private Rect rect;
+
+    void Awake()
     {
-        fpsText = GetComponent<TextMeshProUGUI>();
+        Application.targetFrameRate = 120;
+        QualitySettings.vSyncCount = 0;
+
+        rect = new Rect(10, 10, 400, 200);
+
+        style = new GUIStyle();
+        style.fontSize = 32;
+        style.normal.textColor = Color.white;
     }
 
     void Update()
     {
         deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
-        float fps = 1.0f / deltaTime;
 
-       fpsText.text = "FPS: " + Mathf.Ceil(fps);
-       
-       if (fps >= 60)
-           fpsText.color = Color.green;
-       else if (fps >= 30)
-           fpsText.color = Color.yellow;
-       else
-           fpsText.color = Color.red;
+        fps = 1.0f / deltaTime;
+        frameTimeMs = deltaTime * 1000.0f;
+    }
+
+    void OnGUI()
+    {
+        // Background box
+        GUI.Box(new Rect(5, 5, 450, 180), "");
+
+        // Color based on FPS
+        if (fps >= 60)
+            style.normal.textColor = Color.green;
+        else if (fps >= 30)
+            style.normal.textColor = Color.yellow;
+        else
+            style.normal.textColor = Color.red;
+
+        // Memory Info
+        float totalMemory = Profiler.GetTotalAllocatedMemoryLong() / (1024f * 1024f);
+        float reservedMemory = Profiler.GetTotalReservedMemoryLong() / (1024f * 1024f);
+        float monoMemory = Profiler.GetMonoUsedSizeLong() / (1024f * 1024f);
+
+        string debugText =
+            $"FPS: {Mathf.RoundToInt(fps)}\n" +
+            $"Frame Time: {frameTimeMs:F1} ms\n" +
+            $"Allocated: {totalMemory:F0} MB\n" +
+            $"Reserved: {reservedMemory:F0} MB\n" +
+            $"Mono: {monoMemory:F0} MB";
+
+        GUI.Label(rect, debugText, style);
     }
 }
